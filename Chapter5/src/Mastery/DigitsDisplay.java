f2f4896f50bd0852a3380899db37fcc8313fe4af
49139005 <49139005@L815WD220009.edu.cbe.ab.ca>
2025-11-04 15:31:54 -0700

    3:31pm

--- Chapter5/src/Mastery/DigitsDisplay.java
+++ Chapter5/src/Mastery/DigitsDisplay.java
@@ -4,14 +4,35 @@ public class DigitsDisplay {
 
 	public static void main(String[] args) 
 	{
+		// request number
 		Scanner input = new Scanner(System.in);
-		System.out.print("please enter a none negative intiger: ");
+		System.out.print("please enter a non-negative integer: ");
 		int numb = input.nextInt();
-		
-		String numString = Integer.toString(numb);
-		
+		//change int number to string
+		 String numString = Integer.toString(numb);
+		// divide the number into each individual number and output
+		System.out.println("the digits of the number are: ");
+		for (int i = 0; i < numString.length(); i++) {
+            System.out.println(numString.charAt(i));
+		}
 		
 		
 	}
 
 }
+//Screen dump
+//please enter a non-negative integer: 1515111
+//the digits of the number are: 
+//1
+//5
+//1
+//5
+//1
+//1
+//1
+
+//Screen dump
+//please enter a non-negative integer: 45
+//the digits of the number are: 
+//4
+//5
