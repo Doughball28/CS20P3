ac706cb63a6ea83559456095eeecd9f685ef8c40
49139005 <49139005@L815WD220009.edu.cbe.ab.ca>
2025-11-20 15:29:46 -0700

    3:29PM

--- Chapter7/src/SkillBuilders/Circle.java
+++ Chapter7/src/SkillBuilders/Circle.java
@@ -4,41 +4,66 @@ public class Circle
 {
 	private static final double PI = 3.14;
 	private double radius;
-	
+	private double area;
 	
 	// Default constructor
-		public Circle() 
-		{
-			radius = 1;
-		}
+	public Circle() 
+	{
+		radius = 1;
+		area = 2;
+	}
 
-		// Overloaded constructor that accepts the radius
-		public Circle(double r)
-		{
-			radius = r;
-		}
+	// Overloaded constructor that accepts the radius and area
+	public Circle(double r, double a)
+	{
+		radius = r;
+		area = a;
+	}
 		
-		public double getRadius()
-		{
-			return radius;
-		}
-
+	public double getRadius()
+	{
+		return radius;
+	}
+		
+	public double getarea()
+	{
+		return area;
+	}
 
 	public double area()
 	{
 		double circleArea;
-		
 		circleArea = PI * radius * radius;
-		
 		return circleArea; 
 	}
 	
 	public double circumference()
 	{
 		double c;
-		
 		c = 2 * PI * radius;
-		
 		return c;
+	}
+
+	// toString override
+	@Override
+	public String toString()
+	{
+		return "Circle[radius=" + radius + ", area=" + area + "]";
+	}
+
+	// equals override (no 'this')
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == null)
+			return false;
+
+		if (obj.getClass() != getClass())
+			return false;
+
+		Circle other = (Circle) obj;
+
+		// compare fields
+		return radius == other.radius && area == other.area;
 	}
 }
--- Chapter7/src/SkillBuilders/TestCircle.java
+++ Chapter7/src/SkillBuilders/TestCircle.java
@@ -5,13 +5,25 @@ public class TestCircle {
 	public static void main(String[] args) 
 	{
 
-		// Using the default constructor
+		//default constructor
 		Circle spot = new Circle();
 		System.out.println("Default radius: " + spot.getRadius());
+		System.out.println("Default Area: " + spot.getarea());
+		//overloaded constructor
 		
-		// Using the overloaded constructor
-		Circle dot = new Circle(5);
+		Circle dot = new Circle(5, 10);
 		System.out.println("Overloaded radius: " + dot.getRadius());
-	}
+		System.out.println("Overloaded area: " + dot.getarea());
 
+		System.out.println("spot: " + spot.toString());
+		System.out.println("dot: " + dot.toString());
+				
+		System.out.println("spot equals dot? " + spot.equals(dot));
+				
+		// Another circle identical to spot
+		Circle spot2 = new Circle(1, 2);
+		System.out.println("spot equals spot2? " + spot.equals(spot2));		
+				
+		
+	}
 }
